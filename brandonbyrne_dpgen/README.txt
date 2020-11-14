0c103fe5603c3d6273030f78cfd29c3e8acf59f4
branjb <branjb@gmail.com>
2020-11-14 11:52:24 -0700

    Removed build from filestructure, compiler warnings

---
+++ brandonbyrne_dpgen/README.txt
@@ -0,0 +1,5 @@
+1. Brandon Byrne, NETID: brandonbyrne
+2. ECE 574A
+3. Program that converts a netlist into VHDL
+4. Brandon: 100%
+
--- brandonbyrne_dpgen/build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/branjb/workspace/assignment2/brandonbyrne/googletest/googletest
-# Build directory: /home/branjb/workspace/assignment2/brandonbyrne/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
